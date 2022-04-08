9354c2ad7ebaf542bfae9edc50d310ce535b5ad0
Aiden Sobey <aiden@kzn.io>
2022-04-08 10:57:13 +0800

    Initial commit
    New TypeScript CDK project.
    Going to build our a serverless Data Lake in Go.

---
+++ test/go-go-data-lake.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GoGoDataLake from '../lib/go-go-data-lake-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/go-go-data-lake-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GoGoDataLake.GoGoDataLakeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
